a9d1c8c9e5eef457d61e0267c4f76cde597b984e
AndressaStefany <andressa.stefany@yahoo.com.br>
2017-11-04 16:55:38 -0300

    verificando o tipo do dado

--- loading.cpp
+++ loading.cpp
@@ -3,15 +3,58 @@
 //
 
 #include "loading.h"
+#include <sstream>
 
-Loading::Loading(string arquivo){
+Loading::Loading(string arquivo)
+{
     std::ifstream ifs;
 
-    ifs.open("/home/andressa/Área\ de\ Trabalho/solarsystem/"+arquivo, std::ifstream::in);
+    ifs.open(arquivo, std::ifstream::in);
 
     string buffer;
-    while (ifs >> buffer) {
-        std::cout << buffer << std::endl;
+
+    while (getline(ifs,buffer))
+    {
+        istringstream ss(buffer);
+        ss >> buffer;
+
+        if(buffer == "o")
+        {
+            string objeto;
+            ss >> objeto;
+
+            cout << "objeto: " << objeto << endl;
+        }
+        else if(buffer == "v")
+        {
+            float x[3];
+            ss >> x[0] >> x[1] >> x[2];
+            cout << buffer << " " << x[0] << " " << x[1] << " " << x[2] << endl;
+        }
+        else if(buffer == "vt")
+        {
+            float x[2];
+            ss >> x[0] >> x[1];
+            cout << buffer << " " << x[0] << " " << x[1] << endl;
+        }
+        else if(buffer == "vn")
+        {
+            float x[3];
+            ss >> x[0] >> x[1] >> x[2];
+            cout << buffer << " " << x[0] << " " << x[1] << " " << x[2] << endl;
+        }
+        else if(buffer == "f")
+        {
+            string x[3];
+            ss >> x[0] >> x[1] >> x[2];
+            cout << buffer << " " << x[0] << " " << x[1] << " " << x[2] << endl;
+        }
+        else
+        {
+            cout <<"outra coisa"<<endl;
+
+        }
+
     }
 
     ifs.close();
