bec2c1723b8328d00408695c3fc7b1fa2f21eb43
Vitor Ramos <ramos.vitor89@gmail.com>
2017-11-04 12:23:40 -0300

    rotacao camera

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@
 
 using namespace std;
 
+float dx=0, dy=0, last_x, last_y;
+
 void reshape(int w, int h)
 {
     glViewport(0,0,w,h);
@@ -24,11 +26,42 @@ void display()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+    glLoadIdentity();
+    glTranslatef(0,0,-5);
+    glRotatef(dy, 1,0,0);
+    glRotatef(dx, 0,1,0);
 
+    glPushMatrix();
+    glColor3f(1,0,0);
+    glutSolidCube(1);
+    glPopMatrix();
 
     glutSwapBuffers();
 }
 
+void mouse(int butao, int estado, int x, int y)
+{
+    if(butao == GLUT_LEFT_BUTTON && estado == GLUT_DOWN)
+    {
+        last_x=x;
+        last_y=y;
+    }
+}
+
+void motion(int x, int y)
+{
+    dx+= (x-last_x)*0.5;
+    dy+= (y-last_y)*0.5;
+
+    last_x= x;
+    last_y= y;
+}
+
+void time(int t)
+{
+    glutPostRedisplay();
+    glutTimerFunc(30, time, t);
+}
 
 int main(int argc, char**argv)
 {
@@ -40,6 +73,9 @@ int main(int argc, char**argv)
 
     glutDisplayFunc(display);
     glutReshapeFunc(reshape);
+    glutMouseFunc(mouse);
+    glutMotionFunc(motion);
+    glutTimerFunc(30,time,0);
 
     glClearColor(1,1,1,1);
     glutMainLoop();
